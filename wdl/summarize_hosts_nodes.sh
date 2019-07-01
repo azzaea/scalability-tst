4f1d41ffb38caa3a1c612fd684015a38757dbb77
azzaea <azzaea@uofk.edu>
2019-07-01 07:04:51 +0200

    add wdl nodes analysis script

--- wdl/summarize_hosts_nodes.sh
+++ wdl/summarize_hosts_nodes.sh
@@ -3,5 +3,6 @@ cd hosts
 echo "nodes processes tasks"
 for file in `ls -1v`
 do
-    echo `wc -l $file`| sed 's/_/ /g'
+    nodes=`grep hostwf.logfil $file | sed '2d' | cut -d: -f2 | xargs cat | wc -l`
+    echo $nodes $file | sed 's/_/ /g'
 done
