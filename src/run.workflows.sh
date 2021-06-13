3cc1529de5a143e564bb24ec59f2d84b58b408ac
zo0z <azzaea@uofk.edu>
2021-06-13 09:21:49 -0500

    loop through wfms runs a few times

---
+++ src/run.workflows.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -x
+
+iterations=4
+
+srcDir="/home/a-m/azzaea/scalability-tst/src"
+resultsDir="/home/a-m/azzaea/scalability-tst/results/biocluster.2021"
+
+for (( run=1; run<=${iterations}; run++)); do
+	echo "***************** This is run ${run}/${iterations} *********************"
+	runResultsDir=${resultsDir}/run${run}
+
+	cd ${srcDir}/cwl
+	sbatch --wait bioinfoScaling.toil.sh
+	sbatch --wait bioinfoScaling.cromwell.sh
+	mv cwl.* ${runResultsDir}
+	mv slurm* ${runResultsDir}
+
+	cd ${srcDir}/wdl
+	sbatch --wait bioinfoScaling.cromwell.sh
+	mv wdl.* ${runResultsDir}
+	mv slurm* ${runResultsDir}
+
+	cd ${srcDir}/nf
+	sbatch --wait bioinfoScaling.sh
+	mv nf.* ${runResultsDir}
+	mv slurm* ${runResultsDir}
+done
+
+echo "Bio-Scalability analysis completed for ${iterations} runs" | mail -s "WfMS- Bio-Scalability" "azzaea@gmail.com"
+
