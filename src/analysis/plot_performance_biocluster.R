f5a0a4a1e00b731db505cfcce6858c21e8d42cfd
azzaea <azzaea@gmail.com>
2021-06-26 19:59:17 +0200

    organize local results

--- src/analysis/plot_performance_biocluster.R
+++ src/analysis/plot_performance_biocluster.R
@@ -5,186 +5,204 @@ if (! require(pacman))
 pacman::p_load(tidyverse, scales, viridis, ggthemes, cowplot, rcartocolor)
 
 ## Utility functions -----------------------------------------------------------
-read.perf.results <- function(filename){
-  #filename = perf.files[1]
-  readr::read_csv(filename, 
-                  col_types = cols(cores = col_double(), 
-                                   tasks = col_double(),
-                                   user = col_character(), 
-                                   system = col_double(),
-                                   elapsed = col_double(), 
-                                   cpu = col_character(),
-                                   avMemory = col_double(),
-                                   involuntaryContextSwitch = col_double(),
-                                   voluntaryContextSwitch = col_double(),
-                                   faults = col_double(), 
-                                   inputs = col_double(),
-                                   outputs = col_double(), 
-                                   socketsIn = col_double(),
-                                   socketsOut = col_double(), 
-                                   exitStatus = readr::col_factor())) %>%
+read.perf.results <- function(filename, run = F){
+  #filename = files.perf[1]
+  data <- readr::read_csv(filename, 
+                          col_types = cols(cores = col_double(), 
+                                           tasks = col_double(),
+                                           user = col_character(), 
+                                           system = col_double(),
+                                           elapsed = col_double(), 
+                                           cpu = col_character(),
+                                           avMemory = col_double(),
+                                           involuntaryContextSwitch = 
+                                             col_double(),
+                                           voluntaryContextSwitch = 
+                                             col_double(),
+                                           faults = col_double(), 
+                                           inputs = col_double(),
+                                           outputs = col_double(), 
+                                           socketsIn = col_double(),
+                                           socketsOut = col_double(), 
+                                           exitStatus = readr::col_factor()
+                          )) %>%
     mutate(wf = filename %>% str_remove("/logs.*") %>% basename(), 
-           processes = as.numeric(filename %>% basename() %>% str_extract("\\d"))) 
+           processes = as.numeric(filename %>% basename() %>% 
+                                    str_extract("\\d")))  
+  
+  if (run == T)
+    data <- data %>% 
+      mutate(run = filename %>% str_extract("run\\d") %>% str_remove("run"))
+  
+  return(data)
 }
 
-read.hosts.results <- function(filename){
-  # filename = nodes.files[1]
-  readr::read_delim(filename, delim = " ") %>%
+read.hosts.results <- function(filename, run = F){
+  # filename = files.nodes[1]
+  data <- readr::read_delim(filename, delim = " ") %>%
     mutate(wf = filename %>% str_remove("/summarize.*") %>% basename())
+  
+  if (run == T)
+    data <- data %>% 
+      mutate(run = filename %>% str_extract("run\\d") %>% str_remove("run"))
+  
+  return(data)
 }
+
+plot.figs <- function(data, feature, ylabel, baseline = F){
+  # data = data.perf; feature = "elapsed"; ylabel = "Total runtime (s)"
+  frame <- data %>% 
+    ggplot() + facet_grid(processes ~ .) + theme_bw() 
+  
+  if (baseline)
+    frame <- frame + geom_line(aes(x=tasks, y = theory) , size = 1.5,
+                               color = carto_pal(5, "TealGrn")[3]) 
+  
+  frame +  geom_smooth(aes(x = tasks, y = get(feature), color = wf)) +
+    geom_point(aes(x = tasks, y = get(feature), color = wf, shape = runStatus, 
+                   size = wf_order))  +
+    scale_shape_manual(values = c(19, 8)) +
+    scale_size(range = c(1.75, 2.5), guide = "none") + 
+    scale_color_carto_d(palette = "ag_Sunset") + 
+    guides(color = guide_legend(order = 2)) +   
+    scale_x_log10() + annotation_logticks(sides = 'b') +
+    labs(x = "Number of tasks", y = ylabel, color = str_wrap("Executor", 10)) 
+}
+
 
 ## Basic preps for I/O ---------------------------------------------------------
+rm(list=ls())
 cores.per.node <- 72 # Biocluster results
-results.dir <- "../../results/biocluster.2021"
+results.dir <- "../../results/biocluster.2019/"
 
 figs.dir <- file.path(results.dir, "figs")
 dir.create(figs.dir)
 
 ## Timing and performance ------------------------------------------------------
-perf.files <- list.files(pattern = "^bioinfoScaling_processes", 
+files.perf <- list.files(pattern = "^bioinfoScaling_processes", 
                          path = results.dir, recursive = T, full.names = T)
 
-# perf.files <- list.files(pattern = "^bioinfoScaling_processes", 
+# files.perf <- list.files(pattern = "^bioinfoScaling_processes", 
 #                        path = "../../results/biocluster.2019/", 
 #            recursive = T, full.names = T)
 
-data.perf <- perf.files %>% map_df(read.perf.results) %>% 
+data.perf <- files.perf %>% map_df(read.perf.results, run = F) %>% 
   filter(!is.na(exitStatus)) %>% 
-  mutate(runStatus = case_when(exitStatus == 0 ~ "Success",
-                               exitStatus != 0~ "Failure")) %>%
-  mutate(wf_size = as.numeric(factor(wf, levels = sort(unique(wf), decreasing = T))),
-         processes = as.factor(processes))
+  mutate(runStatus = fct_recode(as.character(exitStatus), 
+                                Success = "0", Failure = "1" )) %>% 
+  mutate(processes = factor(x = processes, 
+                            labels = c("One-step", "Two-step")),
+         cpu = as.double(str_remove(cpu,"%")),
+         wf_order = as.numeric(factor(wf, levels = sort(unique(wf), 
+                                                        decreasing = T) ))) %>% 
+  select(-cores, -user, -system, -avMemory, -faults, -inputs, -outputs, 
+         -socketsIn, -socketsOut, -exitStatus) %>% filter(tasks < 1000)
 
-plot_time <-  data.perf %>% ggplot() + 
-  geom_line(aes(x = cores, y = elapsed, color = wf, linetype = processes)) +
-  geom_point(aes(x = cores, y = elapsed, color = wf, shape = runStatus, 
-                 size = wf_size))  +
-  # geom_rect(data = failed_tasks, aes(xmin = xmin, xmax = xmax, 
-  #                                    ymin = ymin, ymax = max(data.perf$elapsed)), alpha = .4) +
-  scale_x_log10() + annotation_logticks(sides = 'b') +
-  xlab("Number of tasks") +  ylab("Total runtime (s)") + theme_bw() +
-  labs(color = str_wrap("Executor", 10), linetype = "Workflow steps") +
-  guides(color = guide_legend(order = 2)) + 
-  scale_color_carto_d(palette = "ag_Sunset") + 
-  scale_shape_manual(values = c(1, 19, 8)) + 
-  scale_size(range = c(1.75, 2.5), guide = "none") + 
-  #theme(legend.position = "none") +
-  facet_grid(processes~.) + 
-  ggsave(file.path(figs.dir, "Execution_time_split.png"))
 
+
+
+plot_time <-  data.perf %>% #filter(!str_detect(run, '1')) %>% 
+  plot.figs(feature = "elapsed", ylabel = "Total runtime (s)") +
+  theme(legend.position = "none") +
+  ggsave(file.path(figs.dir, "Execution_time.png"))
+  
 plot_time
 
-# speedup <- data.perf %>%
-#   select(elapsed, wf, processes, cores) %>% 
-#   spread(wf, elapsed) %>%
-#   mutate(ratio_wdl = Cromwell_wdl/Nextflow) 
+speedup <- data.perf %>%
+  select(run, wf, processes, tasks, elapsed, runStatus) %>%
+  mutate(elapsed = ifelse(runStatus == "Success", elapsed, NA)) %>% 
+  # filter(!(wf == "nf.nf" & tasks == 2)) %>% 
+  select(-runStatus) %>%
+  pivot_wider(names_from = wf, values_from = elapsed) %>% drop_na(nf.nf) %>% 
+  mutate(across(matches("\\."), .fns = ~ . / nf.nf)) %>% 
+  pivot_longer(matches("\\."), names_to = "wf", values_to = "ratio") %>% 
+  mutate(wf_order = as.numeric(factor(wf, levels = sort(unique(wf), 
+                                                        decreasing = T))),
+         runStatus = fct_recode(as.character(!is.na(ratio)),
+                                Success = "TRUE", Failure = "FALSE" ) %>%
+           fct_inorder(),
+         ratio = case_when(is.na(ratio) ~ 0, TRUE ~ ratio)) 
 
-# plot_speedup <- speedup %>% select(-Nextflow, -contains("Cromwell")) %>%
-#     gather(ratio, value, -cores, -processes) %>% filter(!is.na(value)) %>% ggplot() + 
-#     geom_line(aes(x = cores, y = value, linetype = as.factor(processes),
-#                   color = as.factor(ratio))) +
-#     geom_rect(data = failed_tasks, aes(xmin = xmin, xmax = xmax,
-#                                        ymin = ymin, ymax = ymax/4), alpha = .4) +
-#     scale_x_log10() + annotation_logticks(sides = 'b') +
-#     xlab("Number of tasks") +  ylab("Speed up") + theme_bw() +
-#     labs(color = str_wrap("Workflow management system", 10), linetype = "Workflow steps") +
-#     guides(color = guide_legend(order = 1)) + 
-#     scale_color_carto_d(palette = "ag_Sunset")
-# 
-# plot_speedup
+## 2019 compatible dataframe
+# speedup <- 
+#   data.perf %>%
+#     select(wf, processes, tasks, elapsed, runStatus) %>% 
+#     mutate(elapsed = ifelse(runStatus == "Success", elapsed, NA)) %>% 
+#     filter(!(wf == "nf" & tasks == 2) ) %>% 
+#     filter(!(wf == "nf" & elapsed == 12.85)) %>%
+#     filter(  !(wf == "nf" & elapsed == 12.97)) %>%  
+#     select(-runStatus) %>%
+#   pivot_wider(names_from = wf, values_from = elapsed) %>% 
+#   drop_na(nf) %>% 
+#   mutate(across(matches("\\."), .fns = ~ . / nf)) %>% 
+#   pivot_longer(matches("\\."), names_to = "wf", values_to = "ratio") %>% 
+#   mutate(wf_order = as.numeric(factor(wf, levels = sort(unique(wf), 
+#                                                         decreasing = T))),
+#          runStatus = fct_recode(as.character(!is.na(ratio)),
+#                                 Success = "TRUE" ) %>%
+#            fct_inorder(),
+#          ratio = case_when(is.na(ratio) ~ 0, TRUE ~ ratio)) 
 
-# cowplot::plot_grid(plot_time + theme(legend.position = "none", 
-#                                      axis.title.x = element_blank()), 
-#                    plot_speedup + theme(legend.position = "none"), 
-#                    nrow = 2, rel_heights = c(.75, .25), align = "v") +  
-#   ggsave("Execution_time.png")
+
+plot_speedup <- speedup %>% #filter(runStatus == "Failure") %>% View()
+  plot.figs(feature = "ratio", ylabel = "Speed up") +
+  scale_y_continuous(trans = "pseudo_log") + theme(legend.position = "none") +
+  ggsave(file.path(figs.dir, "Speedup.png"))
+
+plot_speedup
+
+# cowplot::plot_grid(plot_time + theme(legend.position = "none",
+#                                      axis.title.x = element_blank()),
+#                    plot_speedup + theme(legend.position = "none"),
+#                    nrow = 1, rel_heights = c(.75, .25), align = "v") +
+
 
 plot_cpu <- data.perf %>% 
-  mutate(cpu = as.double(str_remove(cpu,"%"))) %>%
-  ggplot() + geom_line(aes(x = cores, y = cpu, color = wf, 
-                           linetype = processes)) +
-  geom_point(aes(x = cores, y = cpu, color = wf, shape = runStatus), size = 2.5) +
-  scale_x_log10() + annotation_logticks(sides = 'b') +
-  xlab("Number of tasks") +  ylab("CPU utilization") + theme_bw() +
-  labs(color = str_wrap("Engine", 10), linetype = "Workflow steps") +
-  guides(color = guide_legend(order = 1)) + 
-  theme(legend.position = "right", legend.box = "vertical") +
-  scale_color_carto_d(palette = "ag_Sunset") + scale_shape_manual(values = c(1, 19, 8)) +
-  facet_grid(processes~.) +
-  ggsave(file.path(figs.dir, "CPU_utilization_split.png"))
+  plot.figs(feature = "cpu", ylabel = "CPU utilization") + 
+  ggsave(file.path(figs.dir, "CPU_utilization.png"))
 
 plot_cpu
 
-plot_involuntaryContextSwitch <- data.perf %>% 
-  ggplot() + geom_line(aes(x = cores, y = involuntaryContextSwitch, 
-                           color = wf, linetype = processes)) +
-  geom_point(aes(x = cores, y = involuntaryContextSwitch, color = wf, 
-                 shape = runStatus), size = 2.5)  +
-  scale_x_log10() + annotation_logticks(sides = 'b') + 
-  xlab("Number of tasks") +  ylab("Involuntary Context Switches") + theme_bw() +
-  labs(color = str_wrap("Engine", 10), linetype = "Workflow steps") +
-  guides(color = guide_legend(order = 1)) + scale_color_carto_d(palette = "ag_Sunset") + 
-  theme(legend.position = "right", legend.box = "vertical") +
-  scale_shape_manual(values = c(1, 19, 8)) +
-  facet_grid(processes~.) +
-  ggsave(file.path(figs.dir, "InvoluntaryContextSwitch_split.png"))
+plot_involuntaryContextSwitch <- data.perf %>% #filter(!str_detect(run, '1')) %>%
+  plot.figs(feature = "involuntaryContextSwitch", 
+            ylabel = "Involuntary Context Switches") + 
+  ggsave(file.path(figs.dir, "InvoluntaryContextSwitch.png"))
 
 plot_involuntaryContextSwitch
 
-plot_voluntaryContextSwitch <- data.perf %>%
-  ggplot() + geom_line(aes(x = cores, y = voluntaryContextSwitch,
-                           color = wf, linetype = processes)) +
-  geom_point(aes(x = cores, y = voluntaryContextSwitch, color = wf,
-                 shape = runStatus), size = 2.5)  +
-  scale_x_log10() + annotation_logticks(sides = 'b') +
-  xlab("Number of tasks") +  ylab("Voluntary Context Switches") + theme_bw() +
-  labs(color = str_wrap("Engine", 10), linetype = "Workflow steps") +
-  guides(color = guide_legend(order = 1)) +   scale_color_carto_d(palette = "ag_Sunset") +
-  theme(legend.position = "right", legend.box = "vertical") +
-  scale_shape_manual(values = c(1, 19, 8)) +
-  facet_grid(processes~.) +
-  ggsave(file.path(figs.dir, "VoluntaryContextSwitch_split.png"))
+plot_voluntaryContextSwitch <- data.perf %>% #filter(!str_detect(run, '1')) %>%
+  plot.figs(feature = "voluntaryContextSwitch", ylabel = "Voluntary Context Switches") + 
+  ggsave(file.path(figs.dir, "VoluntaryContextSwitch.png"))
 
 plot_voluntaryContextSwitch
 
 ## Nodes' allocation -----------------------------------------------------------
 
-nodes.files <- list.files(pattern = "summarize_hosts_nodes.txt", 
+files.nodes <- list.files(pattern = "summarize_hosts_nodes.txt", 
                           path = results.dir, recursive = T, full.names = T)
 
-nodes <- nodes.files %>% map_df(read.hosts.results) %>%
-  mutate(processes = as.factor(as.numeric(str_replace(processes, "host", ""))),
-         tasks = as.numeric(str_replace_all(tasks, "[^0-9]", "")))
+nodes.info <- files.nodes %>% map_df(read.hosts.results, run = T) %>%
+  mutate(tasks = as.numeric(str_replace_all(tasks, "[^0-9]", ""))) %>%
+  mutate(processes = as.numeric(str_replace(processes, "host", "")) %>%
+           factor(x = ., labels = c("One-step", "Two-step"))) %>%
+  filter(tasks < 1000) %>% complete(wf, run, tasks, processes)
 
 
-data.nodes <- left_join(data.perf, nodes, 
+data.nodes <- left_join(data.perf, nodes.info, 
                         by = c("wf" = "wf", "tasks" = "tasks", 
-                               "processes" = "processes" ))  %>% 
-  select(tasks, wf, processes, nodes, runStatus, wf_size) %>% 
+                               # "processes" = "processes", "run" = "run"))  %>%
+                               "processes" = "processes"))  %>% # 2019 compatible
+  select(tasks, wf, processes, nodes, runStatus, wf_order) %>% 
   mutate(theory =ceiling(tasks/cores.per.node), 
          nodes = case_when(!is.na(nodes) ~ nodes,
                            is.na(nodes) ~ 0))
 
-plot_nodes <- data.nodes %>% ggplot() +
-  geom_line(aes(x=tasks, y = theory) , size = 1.5, color = carto_pal(5, "TealGrn")[3]) +
-  geom_line(aes(x=tasks, y = nodes, color =wf, linetype = processes))+#, 
-            #position=position_jitter(w=0.07, h=0)) + 
-  geom_point(aes(x=tasks, y = nodes, color = wf, shape = runStatus, size = wf_size)) +
-  # geom_rect(data = failed_tasks, aes(xmin = xmin, xmax = xmax, 
-  #                                    = ymin, ymax = max(data.nodes$theory)), alpha = .4) +
-  scale_x_log10(name = "Number of tasks", ) +
-  scale_y_continuous(name = "Total occupied nodes", trans = "pseudo_log", limits = c(0,10)) +
-  scale_color_carto_d(palette = "ag_Sunset") +
-  scale_size(range = c(1.75, 3.5), guide = "none") +
-  annotation_logticks(sides = 'b') +
-  theme_bw() + 
-  labs(color = str_wrap("Executor", 10), linetype = "Workflow steps") +
-  guides(color = guide_legend(order = 1)) + 
+plot_nodes <- data.nodes %>% 
+  plot.figs(feature = "nodes", ylabel = "Total occupied nodes", baseline = T) +
+  scale_y_continuous(trans = "pseudo_log", limits = c(0,10)) +
   # annotate("Text", x = 1, y = 10, hjust = 0,
   #          label = "AWS Compute nodes:\n\tType: m5a.24xlarge \n\tCores: 96") +
-  scale_shape_manual(values = c(1, 19, 8)) +
-  facet_grid(processes~.) +
-  ggsave(file.path(figs.dir, "Execution_nodes_split.png"))
+  ggsave(file.path(figs.dir, "Execution_nodes.png"))
 
 plot_nodes
 
@@ -193,9 +211,16 @@ plot_times_nodes <- plot_grid(plot_time + theme(legend.position = "none"),
                               nrow = 1) 
 plot_times_nodes  
 
-legend <- get_legend(plot_time + theme( legend.position = "top", legend.box = "horizontal"))
+legend <- get_legend(plot_time + theme( legend.position = "top", 
+                                        legend.box = "horizontal"))
 
 plot_grid(legend,plot_times_nodes, nrow=2, rel_heights = c(.5,4)) +
-  ggsave(file.path(figs.dir, "times_nodes_split.png"), 
+  ggsave(file.path(figs.dir, "times_nodes.png"), 
          units = "in", width = 10, height = 4.16)
 
+plot_time_speed_nodes <- plot_grid(plot_time, plot_speedup, 
+                                   plot_nodes + theme(legend.position = "none"),
+          nrow = 1)
+plot_grid(legend, plot_time_speed_nodes, nrow = 2, rel_heights = c(.5, 4)) +
+  ggsave(file.path(figs.dir, "times_speed_nodes.png"), 
+         units = "in", width = 10, height = 4.16)
